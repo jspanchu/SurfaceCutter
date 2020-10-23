80c817e8d9afb07cafebcc74b7c63b05ae158fb1
Jaswant Panchumarti <Jaswant.Panchumarti@hal9000>
2020-10-23 19:04:27 +0530

    works. But buggy when duplicate points are present.

--- ExampleSurfaceCutter.cpp
+++ ExampleSurfaceCutter.cpp
@@ -1,19 +1,49 @@
 #include <vtkActor.h>
 #include <vtkCamera.h>
+#include <vtkCommand.h>
 #include <vtkCylinderSource.h>
-#include <vtkNamedColors.h>
 #include <vtkDataSetMapper.h>
+#include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkNamedColors.h>
 #include <vtkProperty.h>
+#include <vtkRenderer.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
-#include <vtkInteractorStyleTrackballCamera.h>
-#include <vtkRenderer.h>
 #include <vtkSmartPointer.h>
+#include <vtkTransform.h>
+#include <vtkTransformFilter.h>
 #include <vtkXMLPolyDataReader.h>
 
 #include <SurfaceCutter.h>
 
 #include <array>
+#include <iostream>
+
+vtkSmartPointer<vtkTransform> meshTransform = vtkSmartPointer<vtkTransform>::New();
+vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
+
+static double rotation = 0;
+
+class RotateCommand : public vtkCommand
+{
+public:
+  vtkTypeMacro(RotateCommand, vtkCommand);
+
+  static RotateCommand* New()
+  {
+    return new RotateCommand;
+  }
+
+  void Execute(vtkObject* caller,
+    unsigned long vtkNotUsed(eventId),
+    void* callData)
+  {
+    //rotation += 0.1;
+    //std::cout << rotation << std::endl;
+    //meshTransform->RotateZ(0.1);
+    renderWindow->Render();
+  }
+};
 
 int main(int, char* []) {
   vtkSmartPointer<vtkNamedColors> colors =
@@ -23,15 +53,20 @@ int main(int, char* []) {
   std::array<unsigned char, 4> bkg{ {26, 51, 102, 255} };
   colors->SetColor("BkgColor", bkg.data());
 
-  auto mesh = vtkSmartPointer<vtkPolyData>::New();
+  //auto mesh = vtkSmartPointer<vtkPolyData>::New();
   auto meshReader = vtkSmartPointer<vtkXMLPolyDataReader>::New();
   meshReader->SetFileName("Testing/triangle.vtp");
+
+  auto meshTransformFilter = vtkSmartPointer<vtkTransformFilter>::New();
+  meshTransformFilter->SetTransform(meshTransform);
+  meshTransform->RotateZ(233.5);
+  meshTransformFilter->SetInputConnection(meshReader->GetOutputPort());
 
   auto polysReader = vtkSmartPointer<vtkXMLPolyDataReader>::New();
   polysReader->SetFileName("Testing/Case5.vtp");
   
   auto surfCutter = vtkSmartPointer<SurfaceCutter>::New();
-  surfCutter->SetInputConnection(0, meshReader->GetOutputPort());
+  surfCutter->SetInputConnection(0, meshTransformFilter->GetOutputPort());
   surfCutter->SetInputConnection(1, polysReader->GetOutputPort());
 
   auto meshMapper = vtkSmartPointer<vtkDataSetMapper>::New();
@@ -58,8 +93,7 @@ int main(int, char* []) {
   renderer->AddActor(polysActor);
   renderer->SetBackground(colors->GetColor3d("BkgColor").GetData());
 
-  auto renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
-  renderWindow->SetSize(1920, 1080);
+  renderWindow->SetSize(640, 480);
   renderWindow->AddRenderer(renderer);
   renderWindow->SetWindowName("Example: SurfaceCutter");
 
@@ -69,6 +103,11 @@ int main(int, char* []) {
   renderWindow->Render();
   auto istyle = vtkSmartPointer<vtkInteractorStyleTrackballCamera>::New();
   renderWindowInteractor->SetInteractorStyle(istyle);
+  renderWindowInteractor->Initialize();
+  renderWindowInteractor->CreateRepeatingTimer(1);
+
+  RotateCommand* rotateCallback = RotateCommand::New();
+  renderWindowInteractor->AddObserver(vtkCommand::KeyPressEvent, rotateCallback);
   renderWindowInteractor->Start();
 
   return EXIT_SUCCESS;
