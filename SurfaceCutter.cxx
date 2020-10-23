a5e3396ee44ad77886cb3cb1405116f1bf4137b8
Jaswant Panchumarti <Jaswant.Panchumarti@hal9000>
2020-10-23 13:07:15 +0530

    Add tests

--- SurfaceCutter.cxx
+++ SurfaceCutter.cxx
@@ -1,5 +1,9 @@
 #include "SurfaceCutter.h"
+#include <vtkDataSet.h>
+#include <vtkInformation.h>
+#include <vtkInformationVector.h>
 #include <vtkObjectFactory.h>
+#include <vtkSmartPointer.h>
 
 vtkStandardNewMacro(SurfaceCutter);
 
@@ -8,6 +12,10 @@ SurfaceCutter::SurfaceCutter()
   this->ComputeBoolean2D = true;
   this->InsideOut = false; // remove portion inside polygons.
   this->TagInsertedPoints = true;
+
+  this->SetNumberOfInputPorts(2);
+  this->SetNumberOfOutputPorts(1);
+
   vtkDebugMacro(<< "Initialized " << this->GetClassNameInternal());
 }
 
@@ -16,8 +24,28 @@ SurfaceCutter::~SurfaceCutter()
   vtkDebugMacro(<< "Destroyed " << this->GetClassNameInternal());
 }
 
-int SurfaceCutter::RequestData(vtkInformation*, vtkInformationVector**, vtkInformationVector*)
+int SurfaceCutter::FillInputPortInformation(int port, vtkInformation* info)
 {
+  switch (port)
+  {
+  case 0:
+    break;
+  case 1:
+    info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkPolyData");
+    break;
+  default:
+    break;
+  }
+}
+
+void SurfaceCutter::SetLoops(vtkDataSet* loops)
+{
+  this->SetInputData(1, loops);
+}
+
+int SurfaceCutter::RequestData(vtkInformation* request, vtkInformationVector** inputVector, vtkInformationVector* outputVector)
+{
+  vtkSmartPointer<vtkDataSet> mesh = vtkDataSet::GetData(inputVector[0]->GetInformationObject(1));
 
   return 1;
 }
---
+++ TestSurfaceCutter.cpp
@@ -0,0 +1,1 @@
+#include <SurfaceCutter.h>
